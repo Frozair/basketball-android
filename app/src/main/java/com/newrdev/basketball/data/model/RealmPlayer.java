c210bc1d67e344a811a245825aa5420a049a81cd
Rudolph Newball <newrdev@gmail.com>
2016-07-20 01:09:21 -0500

    Implementation of the PlayerRepository using Realm

---
+++ app/src/main/java/com/newrdev/basketball/data/model/RealmPlayer.java
@@ -0,0 +1,57 @@
+package com.newrdev.basketball.data.model;
+
+import io.realm.RealmObject;
+import io.realm.annotations.PrimaryKey;
+
+/**
+ * Created by newrdev on 7/20/16.
+ */
+public class RealmPlayer extends RealmObject
+{
+    @PrimaryKey
+    private long id;
+    private String name;
+    private int wonCount;
+    private int lostCount;
+    private int phoneNumber;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getWonCount() {
+        return wonCount;
+    }
+
+    public void setWonCount(int wonCount) {
+        this.wonCount = wonCount;
+    }
+
+    public int getLostCount() {
+        return lostCount;
+    }
+
+    public void setLostCount(int lostCount) {
+        this.lostCount = lostCount;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
---
+++ app/src/main/java/com/newrdev/basketball/data/repository/RealmPlayerRepository.java
@@ -0,0 +1,61 @@
+package com.newrdev.basketball.data.repository;
+
+import com.newrdev.basketball.data.model.Player;
+import com.newrdev.basketball.data.model.RealmPlayer;
+
+import io.realm.Realm;
+
+/**
+ * Created by newrdev on 7/20/16.
+ */
+public class RealmPlayerRepository implements PlayerRepository
+{
+    @Override
+    public void save(Player player)
+    {
+        Realm realm = Realm.getDefaultInstance();
+
+        long id = realm.where(RealmPlayer.class).max("id").longValue();
+        player.setId(Long.toString(id));
+
+        realm.beginTransaction();
+
+        RealmPlayer realmPlayer = realm.createObject(RealmPlayer.class);
+        realmPlayer.setId(id);
+        realmPlayer.setName(player.getName());
+        realmPlayer.setPhoneNumber(player.getPhoneNumber());
+        realmPlayer.setWonCount(player.getWonCount());
+        realmPlayer.setLostCount(player.getLostCount());
+
+        realm.commitTransaction();
+    }
+
+    @Override
+    public void update(Player player)
+    {
+        Realm realm = Realm.getDefaultInstance();
+
+        RealmPlayer realmPlayer = realm.where(RealmPlayer.class).equalTo("id", player.getId()).findFirst();
+
+        realm.beginTransaction();
+
+        realmPlayer.setName(player.getName());
+        realmPlayer.setPhoneNumber(player.getPhoneNumber());
+        realmPlayer.setWonCount(player.getWonCount());
+        realmPlayer.setLostCount(player.getLostCount());
+
+        realm.commitTransaction();
+    }
+
+    @Override
+    public void delete(Player player)
+    {
+        Realm realm = Realm.getDefaultInstance();
+
+        realm.beginTransaction();
+
+        realm.where(RealmPlayer.class).equalTo("id", player.getId()).findFirst().deleteFromRealm();
+
+        realm.commitTransaction();
+    }
+}
